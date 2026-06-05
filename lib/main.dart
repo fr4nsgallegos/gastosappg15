3bd8efc998ad40f02411eaa99713afac24875820
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-06-04 21:42:32 -0500

    product model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gastosappg15/home_page.dart';
+import 'package:gastosappg15/pages/products_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: ProductsPage(), debugShowCheckedModeBanner: false));
 }
---
+++ lib/models/product_model.dart
@@ -0,0 +1,26 @@
+class ProductModel {
+  int? id;
+  String nombre;
+  double precio;
+  int stock;
+
+  ProductModel({
+    this.id,
+    required this.nombre,
+    required this.precio,
+    required this.stock,
+  });
+
+  Map<String, dynamic> tomap() {
+    return {"id": id, "nombre": nombre, "precio": precio, "stock": stock};
+  }
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      id: map["id"],
+      nombre: map["nombre"],
+      precio: map["precio"],
+      stock: map["stocl"],
+    );
+  }
+}
