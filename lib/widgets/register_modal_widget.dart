09357f5e0c1ba4694ea63003dece76c396fe3490
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-06-05 22:00:24 -0500

    actividad generar la info para crear la base de datos

--- lib/widgets/register_modal_widget.dart
+++ lib/widgets/register_modal_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gastosappg15/models/gasto_model.dart';
 import 'package:gastosappg15/utils/data_general.dart';
 import 'package:gastosappg15/widgets/field_widget.dart';
 import 'package:gastosappg15/widgets/item_type_widget.dart';
@@ -15,40 +16,70 @@ class _RegisterModalWidgetState extends State<RegisterModalWidget> {
   TextEditingController priceControler = TextEditingController();
   TextEditingController dateController = TextEditingController();
   String typeSelected = "Alimentos";
+
+  Widget _buildAddButton() {
+    return SizedBox(
+      height: 50,
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+        onPressed: () {
+          GastoModel gastoModel = GastoModel(
+            title: titleContoller.text.trim(),
+            price: double.tryParse(priceControler.text.trim()) ?? 0,
+            dateTime: dateController.text.trim(),
+            type: typeSelected,
+          );
+        },
+        child: Text("Añadir", style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16),
       width: double.infinity,
-      child: Column(
-        children: [
-          Text("Registra tu pago", style: TextStyle(fontSize: 22)),
-          SizedBox(height: 12),
-          FieldWidget(
-            controller: titleContoller,
-            hintText: "Ingresa el título",
-          ),
-          FieldWidget(controller: priceControler, hintText: "Ingresa el monto"),
-          FieldWidget(controller: dateController, hintText: "Ingresa la fecha"),
-          SizedBox(height: 8),
-          Wrap(
-            spacing: 8,
-            runSpacing: 8,
-            alignment: WrapAlignment.center,
-            children: types
-                .map(
-                  (type) => ItemTypeWidget(
-                    data: type,
-                    isSelected: typeSelected == type["name"] ? true : false,
-                    tap: () {
-                      typeSelected = type["name"];
-                      setState(() {});
-                    },
-                  ),
-                )
-                .toList(),
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text("Registra tu pago", style: TextStyle(fontSize: 22)),
+            SizedBox(height: 12),
+            FieldWidget(
+              controller: titleContoller,
+              hintText: "Ingresa el título",
+            ),
+            FieldWidget(
+              controller: priceControler,
+              hintText: "Ingresa el monto",
+            ),
+            FieldWidget(
+              controller: dateController,
+              hintText: "Ingresa la fecha",
+            ),
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              alignment: WrapAlignment.center,
+              children: types
+                  .map(
+                    (type) => ItemTypeWidget(
+                      data: type,
+                      isSelected: typeSelected == type["name"] ? true : false,
+                      tap: () {
+                        typeSelected = type["name"];
+                        setState(() {});
+                      },
+                    ),
+                  )
+                  .toList(),
+            ),
+            SizedBox(height: 8),
+
+            _buildAddButton(),
+          ],
+        ),
       ),
     );
   }
